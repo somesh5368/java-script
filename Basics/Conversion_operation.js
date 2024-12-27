faaf8cdc77c525d2132005c78b968da1250df752
Somesh Pandey <112803292+somesh5368@users.noreply.github.com>
2024-12-27 18:02:31 +0000

    Conversion of variables

---
+++ Basics/Conversion_operation.js
@@ -0,0 +1,33 @@
+let num = "33ag"
+
+//console.log(typeof num);
+//console.log(typeof (num));
+
+let digi = Number(num);
+//console.log(typeof digi);
+
+//console.log(digi)
+
+
+
+//When we try to convert into Number
+
+// "33" => 33
+// "33abc" => NaN
+// Null => 0;
+//True => 1;
+//False => 0;
+//Somesh => NaN; 
+
+
+// When we try to convert into Boolean
+
+let IsBoolean = "";
+
+let NowBoolean = Boolean(IsBoolean);
+
+console.log(NowBoolean);
+
+//1 => true; 0=> false;
+// "" => false;
+//"Somesh" => true;
