34326d430144cadace76685c4d2c1d662c03b97e
Somesh Pandey <112803292+somesh5368@users.noreply.github.com>
2025-02-10 15:07:17 +0000

    new

---
+++ Iteration/04_foreach.js
@@ -0,0 +1,13 @@
+let array =["js","ruby","py","java","node"];
+
+// array.forEach(function(item){
+//     console.log(item);
+// })
+
+
+array.forEach((item)=>{
+    console.log(item);
+})
+
+
+//we use function call back and function name are not mention.
