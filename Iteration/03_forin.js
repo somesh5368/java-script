1ef5b975b45d70101307452f858f9fcf2bb785a6
Somesh Pandey <112803292+somesh5368@users.noreply.github.com>
2025-02-10 14:58:06 +0000

    forof

---
+++ Iteration/03_forin.js
@@ -0,0 +1,16 @@
+let myObj ={
+    "js": "Java Script",
+    "Py": "Python lib",
+    "OOP": "Object oriented programming"
+}
+
+for (const key in myObj) {
+    
+    //console.log(`The shorcut of ${key} is ${myObj[key]}`);
+
+
+    }
+
+
+
+    
