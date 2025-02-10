d5c93e168051b5ecac3396c520eeff51696665b4
Somesh Pandey <112803292+somesh5368@users.noreply.github.com>
2025-02-10 17:24:08 +0000

    array object

--- Iteration/04_foreach.js
+++ Iteration/04_foreach.js
@@ -6,8 +6,35 @@ let array =["js","ruby","py","java","node"];
 
 
 array.forEach((item)=>{
-    console.log(item);
+    //console.log(item);
 })
 
 
 //we use function call back and function name are not mention.
+
+
+
+
+
+let details = [
+    {
+        "name": "Somesh",
+        "color": "red",
+    },
+    {
+        "name": "Nitish",
+        "color": "blue",
+    },
+    {
+        "name": "Ram",
+        "color": "yellow"
+    },
+    {
+        "name": "Shyam",
+        "color": "sky"
+    }
+]
+
+details.forEach(element => {
+    console.log(element["name"])
+});
